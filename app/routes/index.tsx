7fd5faa4f55b25b8f778229e09523f83db4378e6
Dane Stevens <dane@dsmedia.ca>
2023-03-06 11:11:39 -0500

    test

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,54 +1,73 @@
-import { Form, Link, useSearchParams } from "@remix-run/react";
-import { useState } from "react";
+import { Form } from "@remix-run/react";
+import { useEffect, useRef, useState } from "react";
 import { useInterval } from "~/hooks/interval";
 
 const linkStyles = `rounded bg-blue-300 px-4 py-2 text-slate-900 hover:bg-blue-400 mt-8 inline-block`;
 
 export default function Index() {
-  const [searchParams] = useSearchParams();
-  const amount = Number(searchParams.get("amount"));
-  const complete = Boolean(searchParams.get("complete"));
+  const [step, setStep] = useState("DEFAULT");
+  const [amount, setAmount] = useState(0);
+  const audioRef321 = useRef(null);
+  const audioRefCheer = useRef(null);
 
   const [time, setTime] = useState(60);
   useInterval(() => {
-    if (time >= 0 && amount > 0) {
+    if (time >= 0 && step === "REST") {
       setTime((time) => time - 1);
     }
   }, 1000);
+  useEffect(() => {
+    if (step === "REST") {
+      if (time < 4) audioRef321.current.play();
+      if (time < 0) setStep("PUSHUPS");
+    }
+  }, [time]);
   return (
     <div className="bg-slate-900 text-white/90 text-center">
       <div className="max-w-screen-md p-8 h-screen mx-auto flex items-center justify-center">
         <div>
-          {complete && (
+          {step === "COMPLETE" && (
             <div>
               <div className="mb-8 text-7xl">🎉</div>
               <div className="text-4xl">Awesome job!</div>
-              <Link to="/" className={linkStyles}>
+              <button
+                type="button"
+                className={linkStyles}
+                onClick={() => setStep("DEFAULT")}
+              >
                 Reset
-              </Link>
+              </button>
             </div>
           )}
-          {amount > 0 && time < 0 && !complete && (
+          {step === "PUSHUPS" && (
             <div>
               <H2>Step 3/3</H2>
               <H1>Pushups</H1>
               <div className="text-7xl">{Math.round(amount * 1.5)}</div>
-              <Link to="?complete=true" className={linkStyles}>
+              <button
+                type="button"
+                onClick={() => {
+                  setStep("COMPLETE");
+                  return audioRefCheer.current.play();
+                }}
+                className={linkStyles}
+              >
                 Complete
-              </Link>
+              </button>
               <div className="text-left mt-16 bg-white/10 rounded p-4">
                 <H2>Notes</H2>
                 <div className=" mt-2">
                   Your goal is to perform {Math.round(amount * 1.5)} pushups
-                  without ever coming up for a rest! You can "rest" by staying
-                  either in a pushup plank position, shaking out an arm, or by
-                  getting in a downward dog position, just don't let those knees
-                  make contact with the ground again.
+                  (1.5 times your max) without ever coming up for a rest! <br />
+                  <br />
+                  You can "rest" by staying either in a pushup plank position,
+                  shaking out an arm, or by getting in a downward dog position,
+                  just don't let those knees make contact with the ground again.
                 </div>
               </div>
             </div>
           )}
-          {amount > 0 && time >= 0 && !complete && (
+          {step === "REST" && (
             <div>
               <H2>Step 2/3</H2>
               <H1>Rest</H1>
@@ -58,19 +77,29 @@ export default function Index() {
                 <H2>Notes</H2>
                 <div className=" mt-2">
                   Your goal is to perform {Math.round(amount * 1.5)} pushups
-                  without ever coming up for a rest! You can "rest" by staying
-                  either in a pushup plank position, shaking out an arm, or by
-                  getting in a downward dog position, just don't let those knees
-                  make contact with the ground again.
+                  (1.5 times your max) without ever coming up for a rest!
+                  <br />
+                  <br />
+                  You can "rest" by staying either in a pushup plank position,
+                  shaking out an arm, or by getting in a downward dog position,
+                  just don't let those knees make contact with the ground again.
                 </div>
               </div>
             </div>
           )}
-          {amount === 0 && !complete && (
+          {step === "DEFAULT" && (
             <div>
               <H2>Step 1/3</H2>
               <H1>Pushups to failure</H1>
-              <Form method="get">
+              <Form
+                method="get"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  setStep("REST");
+                  audioRef321.current.play();
+                  return audioRef321.current.pause();
+                }}
+              >
                 <div className="my-8">
                   <label>Number of pushups completed</label>
                   <input
@@ -78,6 +107,11 @@ export default function Index() {
                     name="amount"
                     className="block w-full indent-4 rounded border border-slate-800 bg-black/10 outline-none appearance-none py-4"
                     placeholder="Enter a number"
+                    onChange={(e) => setAmount(Number(e.target.value))}
+                    min={1}
+                    max={200}
+                    step={1}
+                    required
                   />
                 </div>
                 <button
@@ -98,6 +132,18 @@ export default function Index() {
           )}
         </div>
       </div>
+      <audio
+        ref={audioRef321}
+        src="/321.mp3"
+        typeof="audio/mp3"
+        preload="true"
+      ></audio>
+      <audio
+        ref={audioRefCheer}
+        src="/cheer.mp3"
+        typeof="audio/mp3"
+        preload="true"
+      ></audio>
     </div>
   );
 }
